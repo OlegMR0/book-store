8106872487db6077c59242de875a95a500c84f5d
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-10-02 02:32:42 +0300

    added service

---
+++ src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
