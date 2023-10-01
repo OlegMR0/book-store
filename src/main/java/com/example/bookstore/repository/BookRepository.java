3ab610520e3558e28edc2238e031c4d004826117
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-10-02 02:14:32 +0300

    added book reposityory and custom exception

---
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.model.Book;
+
+import java.util.List;
+
+public interface BookRepository {
+    Book save(Book book);
+
+    List<Book> findAll();
+
+}
