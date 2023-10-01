9409724bed5506c96649f4e0f3a87eb06a779d4e
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-10-02 02:32:42 +0300

    added service

---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,11 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
