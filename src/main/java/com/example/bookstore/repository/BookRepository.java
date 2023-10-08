c31d9fdb2b063c93e4b2bd0b9614bc8cb0b0b1c3
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-10-08 17:01:09 +0300

    replaced custom repository with Ñspring jpa

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,10 +1,13 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
+
     Book save(Book book);
 
     Book getById(Long id);
