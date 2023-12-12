0ac2f766887e7a7728f4016ad483ee8ec571cb74
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-12-12 02:26:57 +0200

    added user repo, mapper and dto

---
+++ src/main/java/com/example/bookstore/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmail(String email);
+}
