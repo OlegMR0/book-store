f7c1880f40aebe992308ad2d24c5826bef382d6d
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-12-27 02:04:48 +0200

    added public endpoints

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,14 +1,10 @@
 package com.example.bookstore;
 
-import com.example.bookstore.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookStoreApplication {
-    @Autowired
-    private BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
