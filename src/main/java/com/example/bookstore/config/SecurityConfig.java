f37d1ff54ce1498f2c61c54562139b5f539d04c0
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-12-30 03:22:43 +0200

    added role entity and its repository

--- src/main/java/com/example/bookstore/config/SecurityConfig.java
+++ src/main/java/com/example/bookstore/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,8 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableMethodSecurity
 @EnableWebSecurity(debug = true)
+@Configuration
 public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
