0312d518dba4f2d20b804c6507aea25baaf67f2c
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-12-30 04:14:19 +0200

    added user role relation

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     Book save(Book book);
--- src/main/java/com/example/bookstore/repository/RoleRepository.java
+++ src/main/java/com/example/bookstore/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
