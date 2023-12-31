9e3b3cdff37933cf0892cca2af93c0d8a5c2c8a6
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-12-31 03:22:03 +0200

    moved registration logic to separate service

--- src/main/java/com/example/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/bookstore/controller/AuthenticationController.java
@@ -2,16 +2,10 @@ package com.example.bookstore.controller;
 
 import com.example.bookstore.dto.RegisterUserDto;
 import com.example.bookstore.dto.UserResponseDto;
-import com.example.bookstore.dto.mapper.UserMapper;
 import com.example.bookstore.exception.RegistrationException;
-import com.example.bookstore.model.Role;
-import com.example.bookstore.model.User;
-import com.example.bookstore.repository.RoleRepository;
-import com.example.bookstore.repository.UserRepository;
+import com.example.bookstore.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthenticationController {
-    private UserMapper mapper;
-    private UserRepository repository;
-    private PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @PostMapping
     public UserResponseDto register(@RequestBody @Valid RegisterUserDto registerUserDto)
             throws RegistrationException {
-        if (repository.findByEmail(registerUserDto.getEmail()).isPresent()) {
-            throw new RegistrationException("User with this email already exists");
-        }
-        registerUserDto.setPassword(passwordEncoder.encode(registerUserDto.getPassword()));
-        User user = repository.save(mapper.toModel(registerUserDto));
-        return mapper.toResponseDto(user);
+        return userService.register(registerUserDto);
     }
 }
