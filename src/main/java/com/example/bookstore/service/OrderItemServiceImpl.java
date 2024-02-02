f66d129a1709bbc9661cd461329891b4f74e4a92
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2024-02-02 03:28:50 +0200

    start of implementing order and orderItem services

---
+++ src/main/java/com/example/bookstore/service/OrderItemServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.CartItem;
+import com.example.bookstore.model.OrderItem;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderItemServiceImpl implements OrderItemService {
+
+    public OrderItem getOrderItemFromCartItem(CartItem cartItem) {
+
+    }
+}
