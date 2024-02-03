7ceda02a0429e5099ae61f2b3fc6f2409b4b4897
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2024-02-03 03:39:18 +0200

    added repositories, initial controller implementing

--- src/main/java/com/example/bookstore/service/OrderItemServiceImpl.java
+++ src/main/java/com/example/bookstore/service/OrderItemServiceImpl.java
@@ -1,13 +1,25 @@
 package com.example.bookstore.service;
 
+import com.example.bookstore.dto.mapper.OrderItemMapper;
 import com.example.bookstore.model.CartItem;
+import com.example.bookstore.model.Order;
 import com.example.bookstore.model.OrderItem;
+import com.example.bookstore.repository.order.OrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderItemServiceImpl implements OrderItemService {
+    private OrderItemMapper orderItemMapper;
+    private OrderItemRe orderRepository;
 
-    public OrderItem getOrderItemFromCartItem(CartItem cartItem) {
-
+    public OrderItem getOrderItemFromCartItem(CartItem cartItem, Order order) {
+        OrderItem orderItem = orderItemMapper.toOrderItem(cartItem, order);
+        return orderItem;
     }
+
+    @Override
+    public OrderItem save(OrderItem orderItem) {
+        orderRepository.save(orderItem);
+    }
+
 }
