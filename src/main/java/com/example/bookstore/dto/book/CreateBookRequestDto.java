e4b365e92d7c26f1c1a3b5f1abbebe4f3e813e44
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2024-03-12 19:05:00 +0200

    minor fixes

--- src/main/java/com/example/bookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/book/CreateBookRequestDto.java
@@ -11,17 +11,17 @@ import lombok.Data;
 @Data
 public class CreateBookRequestDto {
     @NotBlank
-    @Schema(name = "Book title", example = "Black Raven")
+    @Schema(name = "title", example = "Black Raven")
     private String title;
     @NotBlank
-    @Schema(name = "Book author", example = "Vasyl Shkliar")
+    @Schema(name = "author", example = "Vasyl Shkliar")
     private String author;
     @NotNull
     @Positive
-    @Schema(name = "Book price", example = "150")
+    @Schema(name = "price", example = "150")
     private BigDecimal price;
     @NotBlank
-    @Schema(name = "International Standard Book Number", example = "9781429964371 ")
+    @Schema(name = "isbn", example = "9781429964371 ")
     private String isbn;
     private String description;
     private Set<Long> categoryIds;
